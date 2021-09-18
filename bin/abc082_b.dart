80e5f44507ac789b66e934245c049e43bfebbb3e
yukosugasuga <yukosugasuga@gmail.com>
2021-09-18 21:17:31 +0900

    solve: abc082 b(別解)

--- bin/abc082_b.dart
+++ bin/abc082_b.dart
@@ -4,9 +4,9 @@ void main(List<String> args) {
   var s = stdin.readLineSync().split('').toList();
   var t = stdin.readLineSync().split('').toList();
   s.sort();
-  t.sort();
+  t.sort((a, b) => -1 * a.compareTo(b));
   var sDash = s.join();
-  var tDash = t.reversed.join();
+  var tDash = t.join();
   if (sDash.compareTo(tDash) < 0) {
     print('Yes');
   } else {
