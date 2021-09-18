f7ed54007d80c3c22863f224538b60af2608245b
yukosugasuga <yukosugasuga@gmail.com>
2021-09-18 18:44:35 +0900

    solve: abc081 a(別解)

--- bin/abc081_a.dart
+++ bin/abc081_a.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  var s = stdin.readLineSync().split('').toList();
-  var cnt = 0;
-  for (var si in s) {
-    if (si == '1') {
-      cnt++;
-    }
-  }
+  // var s = stdin.readLineSync().split('').toList();
+  // var cnt = 0;
+  // for (var si in s) {
+  //   if (si == '1') {
+  //     cnt++;
+  //   }
+  // }
+  // print(cnt);
+
+  var s = stdin.readLineSync().split('').map((e) => int.parse(e)).toList();
+  var cnt = s.fold(0, (prev, element) => prev + element);
   print(cnt);
 }
