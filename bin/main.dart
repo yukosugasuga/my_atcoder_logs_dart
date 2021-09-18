dd57bdacb859ccac9c5c88fbd723c475cb4fddd8
yukosugasuga <yukosugasuga@gmail.com>
2021-09-18 17:38:06 +0900

    solve: abc086 a

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  var ab = stdin.readLineSync().split(' ').map((e) => int.parse(e)).toList();
+  var product = ab[0] * ab[1];
+  if (product % 2 == 0) {
+    print('Even');
+  } else {
+    print('Odd');
+  }
 }
